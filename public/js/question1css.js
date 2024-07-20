34924074c9c294973e3436b94a3a353e4f483039
HiagoDaRocha <hiagoferreira436@gmail.com>
2024-07-20 14:00:05 -0300

    Agora coloquei para quando acabar algum valor conseguir colocar outro valor, pois antes quando apagava o valor e tentasse colocar outro não funcionava.

--- public/js/question1css.js
+++ public/js/question1css.js
@@ -1,4 +1,4 @@
-// addEventListener para o evento DOMContentLoaded para garantir que todos os elementos HTML sejam carregados antes da execução do script
+/* addEventListener para o evento DOMContentLoaded para garantir que todos os elementos HTML sejam carregados antes da execução do script */
 document.addEventListener('DOMContentLoaded', function() {
     let textarea = document.querySelector('textarea'); // Seleciona o textarea
     let box = document.getElementById('challenge'); // Seleciona o elemento com ID "challenge"
@@ -9,15 +9,20 @@ document.addEventListener('DOMContentLoaded', function() {
         // Remove espaço em branco do início e fim do texto do textarea
         let styles = textarea.value.trim();
 
-        // Verifica se o texto contém "div" ou "#challenge" seguido de chaves e termina com ponto e vírgula
-        if (styles.match(/^\s*(div|#challenge)\s*\{\s*([^{}]*;\s*)+\s*}\s*$/)) {
+        // Verifica se o texto contém "div" ou "#challenge" seguido de chaves
+        let ValidFormat = styles.match(/^\s*(div|#challenge)\s*\{\s*([^{}]*;\s*)+\s*}\s*$/);
 
+        // Se o formato do texto é válido, aplica os estilos ao elemento com id "challenge"
+        if (ValidFormat) {
+            
             // Extrai o conteúdo entre as chaves
             let cssContent = styles.replace(/^\s*(div|#challenge)\s*\{\s*|\s*}\s*$/g, '');
 
-           // Aplica os estilos extraídos ao elemento com ID "challenge"
-            box.style.cssText = cssContent;
+            // Limpa os estilos anteriores
+            box.style.cssText = '';
 
+            // Aplica os novos estilos
+            box.style.cssText = cssContent;
         }
     });
 });
